973d530553d76f4e143ca1a0e53222aa5c5dd2df
universato <universato@gmail.com>
2024-07-14 00:57:43 +0900

    Update

--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
+from aqt import dialogs
 from aqt import gui_hooks, mw
 from aqt.qt import QAction
-import webbrowser
-import os
 from datetime import datetime, timedelta
+import os
+import webbrowser
 
 def google_search()-> None:
     webbrowser.open(f"https://www.google.com/search?q={selected_text()}")
@@ -12,6 +13,11 @@ def google_image_search() -> None:
 
 def open_wikipedia()-> None:
     webbrowser.open(f"https://ja.wikipedia.org/wiki/{selected_text()}")
+
+def search_in_collection():
+    browser = dialogs.open("Browser", mw)
+    browser.form.searchEdit.lineEdit().setText(selected_text())
+    browser.onSearchActivated()
 
 def open_pixiv()-> None:
     webbrowser.open(f"https://dic.pixiv.net/a/{selected_text()}")
@@ -31,7 +37,7 @@ def selected_text() -> str:
     obj = mw.app.activeWindow()
     if hasattr(obj,'editor'):
         obj = obj.editor
-    return obj.web.selectedText()
+    return obj.web.selectedText().strip()
 
 def log_message(message:str) -> None:
     addon_folder = os.path.dirname(__file__)
@@ -41,36 +47,23 @@ def log_message(message:str) -> None:
     # with open(log_file, "a", encoding="utf-8") as f:
     #     f.write(message + "\n")
 
-def on_context_menu(_webview, menu)-> None:
-    if selected_text().strip():
-        action = QAction("Googleで検索", menu)
-        action.triggered.connect(google_search)
-        menu.addAction(action)
+def on_context_menu(_webview, menu) -> None:
+    if selected_text():
+        actions = [
+            ("Googleで検索", google_search),
+            ("Googleで画像を検索", google_image_search),
+            ("Wikipedia", open_wikipedia),
+            ("Anki内で検索", search_in_collection),
+            ("Pixiv大百科", open_pixiv),
+            ("Twitter", twitter_search),
+            ("ニコニコ大百科", open_niconico_pedia),
+            ("アンサイクロペディア", open_ansaikuropedia),
+        ]
 
-        action2 = QAction("Googleで画像を検索", menu)
-        action2.triggered.connect(google_image_search)
-        menu.addAction(action2)
-
-        action3 = QAction("Wikipedia", menu)
-        action3.triggered.connect(open_wikipedia)
-        menu.addAction(action3)
-
-        action4 = QAction("Pixiv大百科", menu)
-        action4.triggered.connect(open_pixiv)
-        menu.addAction(action4)
-
-        action5 = QAction("Twitter", menu)
-        action5.triggered.connect(twitter_search)
-        menu.addAction(action5)
-
-        action6 = QAction("ニコニコ大百科", menu)
-        action6.triggered.connect(open_niconico_pedia)
-        menu.addAction(action6)
-
-        action7 = QAction("アンサイクロペディア", menu)
-        action7.triggered.connect(open_ansaikuropedia)
-        menu.addAction(action7)
+        for title, func in actions:
+            action = QAction(title, menu)
+            action.triggered.connect(func)
+            menu.addAction(action)
 
 gui_hooks.editor_will_show_context_menu.append(on_context_menu) # Editor
 gui_hooks.webview_will_show_context_menu.append(on_context_menu) # Problem
-# log_message("GoogleSearchAddonがロードされました")
